c2fa5035d1ebf3683c985e0114f78bdff053c7c1
Yinan Xu <xuyinan@ict.ac.cn>
2022-12-10 16:49:57 +0800

    lsu: enable store diff for AMO instructions

--- src/main/scala/nutcore/backend/fu/UnpipelinedLSU.scala
+++ src/main/scala/nutcore/backend/fu/UnpipelinedLSU.scala
@@ -321,6 +321,7 @@ class UnpipelinedLSU extends NutCoreModule with HasLSUConst {
   io.loadAccessFault := lsExecUnit.io.loadAccessFault
   io.storeAccessFault := lsExecUnit.io.storeAccessFault || hasScAccessFault
 
+  val isStore = io.out.fire && (state === s_idle || state === s_sc || state === s_amo_s)
   val isMemStore = RegEnable(io.dmem.req.bits.cmd === SimpleBusCmd.write && io.dmem.req.bits.addr >= 0x80000000L.U, io.dmem.req.fire)
   val storeAddr = RegEnable(io.dmem.req.bits.addr, io.dmem.req.fire)
   val storeData = RegEnable((MaskExpand(io.dmem.req.bits.wmask) & io.dmem.req.bits.wdata).asUInt, io.dmem.req.fire)
@@ -329,7 +330,7 @@ class UnpipelinedLSU extends NutCoreModule with HasLSUConst {
   difftest.clock  := clock
   difftest.coreid := 0.U
   difftest.index  := 0.U
-  difftest.valid  := RegNext(RegNext(RegNext(io.out.fire && state === s_idle && isMemStore)))
+  difftest.valid  := RegNext(RegNext(RegNext(isStore && isMemStore)))
   difftest.addr   := RegNext(RegNext(RegNext(storeAddr)))
   difftest.data   := RegNext(RegNext(RegNext(storeData)))
   difftest.mask   := RegNext(RegNext(RegNext(storeMask)))
