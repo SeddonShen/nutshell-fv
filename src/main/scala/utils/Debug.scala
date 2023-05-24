ad4b67b6334789df77ee8283a63f00b8bcf68908
Yinan Xu <xuyinan@ict.ac.cn>
2023-05-24 11:05:43 +0800

    Remove debug printf

--- src/main/scala/utils/Debug.scala
+++ src/main/scala/utils/Debug.scala
@@ -2,17 +2,17 @@
 /**************************************************************************************
 * Copyright (c) 2020 Institute of Computing Technology, CAS
 * Copyright (c) 2020 University of Chinese Academy of Sciences
-* 
-* NutShell is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2. 
-* You may obtain a copy of Mulan PSL v2 at:
-*             http://license.coscl.org.cn/MulanPSL2 
-* 
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER 
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR 
-* FIT FOR A PARTICULAR PURPOSE.  
 *
-* See the Mulan PSL v2 for more details.  
+* NutShell is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*             http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER
+* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR
+* FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
 ***************************************************************************************/
 
 package utils
@@ -39,7 +39,7 @@ object LogUtil {
 
   def displayLog: Bool = {
     val enableDisplay = WireInit(false.B)
-    BoringUtils.addSink(enableDisplay, "DISPLAY_ENABLE")
+    // BoringUtils.addSink(enableDisplay, "DISPLAY_ENABLE")
     enableDisplay
   }
 
@@ -52,10 +52,14 @@ object LogUtil {
   def apply(debugLevel: LogLevel)
            (prefix: Boolean, cond: Bool, pable: Printable)
            (implicit name: String): Any = {
-    val commonInfo = p"[${GTimer()}] $name: "
-    when (false.B && cond && displayLog) {
-      if(prefix) printf(commonInfo)
-      printf(pable)
+    if (false) {
+      when (cond && displayLog) {
+        if (prefix) {
+          val commonInfo = p"[${GTimer()}] $name: "
+          printf(commonInfo)
+        }
+        printf(pable)
+      }
     }
   }
 }
